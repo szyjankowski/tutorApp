a26782781e908459dd6b7363c3f34e7af89bd24a
szjankowski <szymonchesstry@gmail.com>
2023-11-01 00:41:22 +0100

    feat: add custom user model, with tests, and slight changes to admin panel

--- users/models.py
+++ users/models.py
@@ -1,19 +1,30 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy
+
+from users.managers import CustomUserManager
 
 
-class CustomUser(AbstractUser):
-    email = models.EmailField(null=False, unique=True)
-    password = models.CharField()
-    #dodac reszte
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(gettext_lazy("email adress"), unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = "email"
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
 
 
 class UserProfile(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='profile')
     profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
-    description = models.TextField(null=True, blank=True)
+    description = models.TextField(blank=True)
 
 
 class StudentProfile(UserProfile):
-    grade = models.CharField(max_length=255, null=True, blank=True)
+    grade = models.CharField(max_length=255, blank=True)
